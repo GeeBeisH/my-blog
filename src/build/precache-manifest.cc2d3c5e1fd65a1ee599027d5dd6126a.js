6b0c589474767627ca0823db8bc4f39c89b99642
Chris Wessels <geebeish@mweb.co.za>
2019-12-12 10:07:19 +0200

    First Commit

---
+++ src/build/precache-manifest.cc2d3c5e1fd65a1ee599027d5dd6126a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cea360c45a4f8fc923ffb581ab0e18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3c61f90b682fe5eb619",
+    "url": "/static/css/main.09973adf.chunk.css"
+  },
+  {
+    "revision": "786e5878e64090fff635",
+    "url": "/static/js/2.7c275462.chunk.js"
+  },
+  {
+    "revision": "d3c61f90b682fe5eb619",
+    "url": "/static/js/main.298c683c.chunk.js"
+  },
+  {
+    "revision": "1ec3af87831298deb3c8",
+    "url": "/static/js/runtime-main.32a9f1c2.js"
+  }
+]);
